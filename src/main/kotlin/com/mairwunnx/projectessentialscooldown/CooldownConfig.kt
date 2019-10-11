1d68050976d055577eb40031f26088ea866b5dee
Pavel Erokhin <MairwunNx@gmail.com>
2019-10-11 20:45:54 +0300

    Added loading and saving cooldown config.

---
+++ src/main/kotlin/com/mairwunnx/projectessentialscooldown/CooldownConfig.kt
@@ -0,0 +1,67 @@
+package com.mairwunnx.projectessentialscooldown
+
+import com.mairwunnx.projectessentialscooldown.models.CooldownModel
+import com.mairwunnx.projectessentialscore.extensions.empty
+import com.mairwunnx.projectessentialscore.helpers.MOD_CONFIG_FOLDER
+import kotlinx.serialization.UnstableDefault
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonConfiguration
+import org.apache.logging.log4j.LogManager
+import java.io.File
+import java.io.FileNotFoundException
+
+@UseExperimental(UnstableDefault::class)
+internal object CooldownConfig {
+    private val logger = LogManager.getLogger()
+    internal var config = CooldownModel()
+    private val COOLDOWNS_CONFIG = MOD_CONFIG_FOLDER + File.separator + "cooldowns.json"
+
+    internal fun load() {
+        logger.info("    - loading command cooldown configuration ...")
+        var checkingCooldown = String.empty
+        try {
+            val configRaw = File(COOLDOWNS_CONFIG).readText()
+            config = Json.parse(CooldownModel.serializer(), configRaw)
+            logger.info("    - loaded cooldowns (${config.commandCooldowns.size})")
+            config.commandCooldowns.forEach {
+                checkingCooldown = it
+                val command = it.split("=")[0]
+                val cooldown = it.split("=")[1]
+                logger.info("        - command: ${command}; cooldown: $cooldown")
+            }
+        } catch (ex: FileNotFoundException) {
+            logger.error("Configuration file ($COOLDOWNS_CONFIG) not found!")
+            logger.warn("The default configuration will be used.")
+        } catch (index: IndexOutOfBoundsException) {
+            logger.error("Cooldown $checkingCooldown loaded with error, please check your configuration.")
+        }
+    }
+
+    internal fun save() {
+        logger.debug("        - setup json configuration for parsing ...")
+        val json = Json(
+            JsonConfiguration(
+                encodeDefaults = true,
+                strictMode = true,
+                unquoted = false,
+                allowStructuredMapKeys = true,
+                prettyPrint = true,
+                useArrayPolymorphism = false
+            )
+        )
+        createConfigDirs()
+        logger.info("        - saving command cooldowns configuration ...")
+        val configRaw = json.stringify(CooldownModel.serializer(), config)
+        try {
+            File(COOLDOWNS_CONFIG).writeText(configRaw)
+        } catch (ex: SecurityException) {
+            logger.error("An error occurred while saving cooldowns configuration", ex)
+        }
+    }
+
+    private fun createConfigDirs() {
+        logger.info("        - creating directories for configurations ...")
+        val configDirectory = File(MOD_CONFIG_FOLDER)
+        if (!configDirectory.exists()) configDirectory.mkdirs()
+    }
+}
--- src/main/kotlin/com/mairwunnx/projectessentialscooldown/EntryPoint.kt
+++ src/main/kotlin/com/mairwunnx/projectessentialscooldown/EntryPoint.kt
@@ -1,8 +1,15 @@
 package com.mairwunnx.projectessentialscooldown
 
 import com.mairwunnx.projectessentialscore.EssBase
+import com.mairwunnx.projectessentialscore.extensions.commandName
+import com.mairwunnx.projectessentialscore.extensions.player
+import net.minecraft.entity.player.ServerPlayerEntity
 import net.minecraftforge.common.MinecraftForge
+import net.minecraftforge.event.CommandEvent
+import net.minecraftforge.eventbus.api.EventPriority
+import net.minecraftforge.eventbus.api.SubscribeEvent
 import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.event.server.FMLServerStoppingEvent
 import org.apache.logging.log4j.LogManager
 
 @Suppress("unused")
@@ -18,9 +25,27 @@ internal class EntryPoint : EssBase() {
         logger.debug("Register event bus for $modName mod ...")
         MinecraftForge.EVENT_BUS.register(this)
         logger.info("Loading $modName permissions data ...")
+        CooldownConfig.load()
     }
 
     internal companion object {
         internal lateinit var modInstance: EntryPoint
+    }
+
+    @SubscribeEvent(priority = EventPriority.HIGH)
+    internal fun onPlayerCommand(event: CommandEvent) {
+        if (event.parseResults.context.source.entity is ServerPlayerEntity) {
+            val commandName = event.commandName
+            val commandSender = event.player
+            val commandSenderNickName = commandSender.name.string
+        }
+    }
+
+    @Suppress("UNUSED_PARAMETER")
+    @SubscribeEvent
+    fun onServerStopping(it: FMLServerStoppingEvent) {
+        logger.info("Shutting down $modName mod ...")
+        logger.info("    - Saving cooldown configuration ...")
+        CooldownConfig.save()
     }
 }
